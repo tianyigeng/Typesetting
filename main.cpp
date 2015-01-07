8006a24c5eb7286fa61d589d0cf497d468997dd4
TarnumG <gengty1995@126.com>
2015-01-07 19:58:56 +0800

    Not completed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 /*
 	This is a C++ implement of the beautiful typesetting problem. 
+    Dynamic methods. 
 	Copyright @ Mr. Geng Tianyi
 	Contact: gty12[at]mails[dot]tsinghua[dot]edu[dot]cn. 
 */
@@ -15,54 +16,38 @@ public:
     vector<int> word;
     vector<int> optiValuePre;
     vector<int> optiValuePost;
-    vector<int> cut_point; // Index of beginning words in all lines.
+    vector<int> cut_point_post; // Index of ending words in all lines @last paragraph.
+    vector<int> cut_point_pre; // Index of ending words in all lines @first paragraph.
 
     Typesetting();
     ~Typesetting();
 
     bool FindPostValue(); // Implement the optiValuePost array. 
     bool FindPreValue(); // Implement the optiValuePre array. 
-    bool FindSolution();
+    void FindSolution();
 };
+
+void Typesetting::FindSolution() {
+    // 
+    return ;
+}
+
+Typesetting::Typesetting() {
+    int temp = 0;
+    cin >> num >> max_len; 
+    for (int i = 0; i < maxlen; ++i) {
+        cin >> temp;
+        word.push_back(temp);
+    }
+    return ;
+}
+
+Typesetting::~Typesetting() {
+    return ;
+}
 
 bool Typesetting::FindPreValue() {
 	if (!optiValuePre.length() == word.length()) { // Sanity check
-		return false;
-	}
-
-	// Search from backward to forward 
-	int curr_word = 0; // Index of current word
-    int curr_len = 0; // Length of current line
-
-    // If all words can be put in a single line
-    while (curr_len + word[curr_word - 1] + 1 <= max_len) {
-    	curr_word--;
-    	curr_len += word[curr_word] + 1; 
-    	optiValuePost[curr_word] = 0; 
-    	cut_point[curr_word] = num - 1;
-    }
-
-    // Dynamic begins
-    for (int i = curr_word - 1; i >= 0; --i) {
-    	int temp_cut_pos = i;
-    	curr_len = word[i];
-
-    	// New words can be added to line begin with word[i]
-    	while (curr_len + word[temp_cut_pos + 1] + 1 <= max_len) {
-    		temp_cut_pos++;
-    		curr_len += word[temp_cut_pos] + 1; 
-			int temp_P_val = (max_len - curr_len)^2 + optiValuePost[temp_cut_pos + 1];
-			// If the current cut position is better
-    		if (temp_P_val < optiValuePost[i]) {
-    			optiValuePost[i] = temp_P_val;
-    			cut_point[i] = temp_cut_pos;
-    		} //!!!!
-    	}
-
-    }
-
-    bool Typesetting::FindPostValue() {
-	if (!optiValuePost.length() == word.length()) { // Sanity check
 		return false;
 	}
 
@@ -75,7 +60,7 @@ bool Typesetting::FindPreValue() {
     	curr_word--;
     	curr_len += word[curr_word] + 1; 
     	optiValuePost[curr_word] = 0; 
-    	cut_point[curr_word] = num - 1;
+    	cut_point_p[curr_word] = num - 1;
     }
 
     // Dynamic begins
@@ -91,7 +76,43 @@ bool Typesetting::FindPreValue() {
 			// If the current cut position is better
     		if (temp_P_val < optiValuePost[i]) {
     			optiValuePost[i] = temp_P_val;
-    			cut_point[i] = temp_cut_pos;
+    			cut_point_post[i] = temp_cut_pos;
+    		} //!!!!
+    	}
+
+    }
+
+bool Typesetting::FindPostValue() {
+	if (!optiValuePost.length() == word.length()) { // Sanity check
+		return false;
+	}
+
+	// Search from backward to forward 
+	int curr_word = -1; // Index of current word
+    int curr_len = 0; // Length of current line
+
+    // If all words can be put in a single line
+    while (curr_len + word[curr_word + 1] + 1 <= max_len) {
+    	curr_word++;
+    	curr_len += word[curr_word] + 1; 
+    	optiValuePost[curr_word] = 0; 
+    	// cut_point_post[curr_word] = 0;
+    }
+
+    // Dynamic begins
+    for (int i = curr_word + 1; i < num; ++i) {
+    	int temp_cut_pos = i;
+    	curr_len = word[i];
+
+    	// New words can be added to line begin with word[i]
+    	while (curr_len + word[temp_cut_pos + 1] + 1 <= max_len) {
+    		temp_cut_pos++;
+    		curr_len += word[temp_cut_pos] + 1; 
+			int temp_P_val = (max_len - curr_len)^2 + optiValuePost[temp_cut_pos + 1];
+			// If the current cut position is better
+    		if (temp_P_val < optiValuePost[i]) {
+    			optiValuePost[i] = temp_P_val;
+    			cut_point_post[i] = temp_cut_pos;
     		} //!!!!
     	}
 
@@ -110,13 +131,8 @@ bool Typesetting::FindPreValue() {
 int main() {
     
 
-    cin >> num >> max_len;
-
-     
-
-    for (int i = 0; i < num; ++i) {
-    	cin >> word[i];
-    }
+    Typesetting prob;
+    prob.solve();
 
 
 
